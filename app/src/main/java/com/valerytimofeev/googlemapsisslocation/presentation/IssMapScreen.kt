e63af8aa6eb1a0acaa1c00b61accca0f25c046fa
llin first <90470991+Haartag@users.noreply.github.com>
2022-06-17 22:35:19 +0300

    Custom marker, center map on marker

--- app/src/main/java/com/valerytimofeev/googlemapsisslocation/presentation/IssMapScreen.kt
+++ app/src/main/java/com/valerytimofeev/googlemapsisslocation/presentation/IssMapScreen.kt
@@ -1,16 +1,25 @@
 package com.valerytimofeev.googlemapsisslocation.presentation
 
+import android.graphics.Bitmap
 import android.util.Log
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Lock
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.rememberCameraPositionState
+import com.valerytimofeev.googlemapsisslocation.R
 import com.valerytimofeev.googlemapsisslocation.data.remote.responses.IssLocationResponse
 import com.valerytimofeev.googlemapsisslocation.data.remote.toIssLocation
 import com.valerytimofeev.googlemapsisslocation.utility.Constants.TAG
@@ -28,16 +37,22 @@ fun MapSurface(
         ) {
             value = viewModel.loadIss()
         }
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(LatLng(0.0, 0.0), 2f)
+    }
 
     Scaffold(scaffoldState = scaffoldState) {
         GoogleMap(
             modifier = Modifier.fillMaxSize(),
-            properties = viewModel.mapProperties
+            properties = viewModel.mapProperties,
+            cameraPositionState = cameraPositionState
         ) {
             if (locationState.value is IssLocationResource.Success) {
+                //center map on marker
+                cameraPositionState.position = viewModel.getCameraPosition(locationState.value.data!!.toIssLocation().latLng)
                 Marker(
                     position = locationState.value.data!!.toIssLocation().latLng,
-                    icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE),
+                    icon = BitmapDescriptorFactory.fromBitmap(viewModel.getMarkerBitmap(LocalContext.current)),
                     title = "Spot (${locationState.value.data!!.toIssLocation().latLng})",
                     onClick = {
                         it.showInfoWindow()
--- app/src/main/java/com/valerytimofeev/googlemapsisslocation/presentation/IssMapViewModel.kt
+++ app/src/main/java/com/valerytimofeev/googlemapsisslocation/presentation/IssMapViewModel.kt
@@ -1,11 +1,19 @@
 package com.valerytimofeev.googlemapsisslocation.presentation
 
+import android.content.Context
+import android.graphics.Bitmap
 import android.util.Log
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.maps.android.compose.MapProperties
+import com.valerytimofeev.googlemapsisslocation.R
 import com.valerytimofeev.googlemapsisslocation.data.remote.responses.IssLocationResponse
 import com.valerytimofeev.googlemapsisslocation.domain.repository.IssLocationRepository
 import com.valerytimofeev.googlemapsisslocation.utility.Constants
@@ -25,6 +33,26 @@ class IssMapViewModel @Inject constructor(
 
     init {
         Log.d(Constants.TAG, "init")
+    }
+
+    //make custom icon from resource
+    fun getMarkerBitmap(context: Context): Bitmap {
+        val markerVector =
+            ContextCompat.getDrawable(context, R.drawable.ic_launcher_foreground)!!
+        markerVector.setBounds(0, 0, markerVector.intrinsicWidth, markerVector.intrinsicHeight)
+        val markerBitmap = Bitmap.createBitmap(
+            markerVector.intrinsicWidth,
+            markerVector.intrinsicHeight,
+            Bitmap.Config.ARGB_8888
+        )
+        val canvas = android.graphics.Canvas(markerBitmap)
+        markerVector.draw(canvas)
+        return markerBitmap
+    }
+
+    //make CameraPosition from Iss LatLng
+    fun getCameraPosition(position: LatLng): CameraPosition {
+        return CameraPosition.fromLatLngZoom(position, 2f)
     }
 
     //API test part
